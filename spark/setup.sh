384028f7288e48eb7f8ef8eddc2031cfbad689ee
harshcs09 <harsh.cs09@gmail.com>
2015-10-29 15:10:57 +0530

    Linking custom hadoop to spark

--- spark/setup.sh
+++ spark/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
+echo "export HADOOP_HOME=/root/hadoop" >> /root/spark/conf/spark-env.sh
+echo "SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)" >> /root/spark/conf/spark-env.sh
+echo "SPARK_DIST_CLASSPATH=$SPARK_DIST_CLASSPATH:$HADOOP_HOME/share/hadoop/tools/lib/*" >> /root/spark/conf/spark-env.sh
+echo "HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop/" >> /root/spark/conf/spark-env.sh
+echo "export JAVA_LIBRARY_PATH=$HADOOP_HOME/lib/native:$JAVA_LIBRARY_PATH" >> /root/spark/conf/spark-env.sh
 /root/spark-ec2/copy-dir /root/spark

f49d9090eaa5b298d69ea78c8b6088fd69d2992f
harshcs09 <harsh.cs09@gmail.com>
2015-10-30 12:49:14 +0530

    Update init.sh

--- spark/init.sh
+++ spark/init.sh
@@ -145,7 +145,6 @@ else
   tar xvzf spark-*.tgz > /tmp/spark-ec2_spark.log
   rm spark-*.tgz
   mv `ls -d spark-* | grep -v ec2` spark
-  mkdir -p hadoop_new
   mv `ls -d hadoop-* | grep -v native` hadoop_new
 fi
 

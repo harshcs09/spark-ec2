1bd1d4f0db0d16a94bb4292fd354843ed1705dc7
harshcs09 <harsh.cs09@gmail.com>
2015-10-30 13:21:25 +0530

    Update setup.sh

--- spark/setup.sh
+++ spark/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "export HADOOP_HOME=/root/hadoop_new" >> /root/spark/conf/spark-env.sh
-echo "SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)" >> /root/spark/conf/spark-env.sh
-echo "SPARK_DIST_CLASSPATH=$SPARK_DIST_CLASSPATH:$HADOOP_HOME/share/hadoop/tools/lib/*" >> /root/spark/conf/spark-env.sh
-echo "HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop/" >> /root/spark/conf/spark-env.sh
-echo "export JAVA_LIBRARY_PATH=$HADOOP_HOME/lib/native:$JAVA_LIBRARY_PATH" >> /root/spark/conf/spark-env.sh
+#echo "export HADOOP_HOME=/root/hadoop_new" >> /root/spark/conf/spark-env.sh
+#echo "SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)" >> /root/spark/conf/spark-env.sh
+#echo "SPARK_DIST_CLASSPATH=$SPARK_DIST_CLASSPATH:$HADOOP_HOME/share/hadoop/tools/lib/*" >> /root/spark/conf/spark-env.sh
+#echo "HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop/" >> /root/spark/conf/spark-env.sh
+#echo "export JAVA_LIBRARY_PATH=$HADOOP_HOME/lib/native:$JAVA_LIBRARY_PATH" >> /root/spark/conf/spark-env.sh
 /root/spark-ec2/copy-dir /root/spark
-/root/spark-ec2/copy-dir /root/hadoop_new
+#/root/spark-ec2/copy-dir /root/hadoop_new
